d8a528d806036f82228cf23415e9011f59def437
rctauber <rctauber@gmail.com>
2020-06-18 14:23:51 -0700

    Move create into init

--- README.md
+++ README.md
@@ -10,47 +10,33 @@ COGS takes a set of TSV files on your local file system and allows you to edit t
 Since COGS is designed to synchronize local and remote sets of tables,
 we try to follow the familiar `git` interface and workflow:
 
-- `cogs init` creates a `.cogs/` directory to store configuration data and copies of the remote files
-- `cogs create` creates a Google Sheet and stores the ID in `.cogs/`
+- `cogs init` creates a `.cogs/` directory to store configuration data and creates a Google Sheet for the project
 - `cogs add foo.tsv` starts tracking the `foo.tsv` table
 - `cogs push` pushes local tables to the Google Sheet
 - `cogs fetch` fetches the data from the Goolgle Sheet and stores it in `.cogs/`
 - `cogs status` summarizes the differences between tracked files and their copies in `.cogs/`
 - `cogs diff` shows detailed differences between local files and the Google Sheet
 - `cogs pull` overwrites local files with the data from the Google Sheet, if they have changed
-- `cogs delete` destroys the Google Sheet, but leaves local files alone
+- `cogs delete` destroys the Google Sheet and configuration data, but leaves local files alone
 
 There is no step corresponding to `git commit`.
 
 ### `init`
 
-```
-cogs init -c [path-to-credentials] -u [email] -r [role]
-```
-
-`init` **requires** [Google API credentials](https://gspread.readthedocs.io/en/latest/oauth2.html#enable-api-access-for-a-project) in JSON format to be used for all sheet operations (`-c`/`-credentials`). These credentials are used to create a [`gspread`](https://gspread.readthedocs.io/en/latest/) Client that can create, share, edit, and delete Google Sheets.
-
-Optionally, you may provide a user email (`-u`/`--user`) that all sheets will be shared with when running `create`. By default, this user will have write access. You can also specify a role (`-r`/`--role`) to override this:
-- `owner` (there can only be one owner of a sheet, and any time the `owner` role is specified, the ownership will be transferred to that user)
-- `writer`
-- `viewer`
-
-You can also share with multiple users by providing the path to a TSV (`-U`, `--users`). Each line should have a user email and the role for that user, separated by a tab.
-
-Six files are created in the `.cogs/` directory when running `init`:
-- `config.tsv`: COGS configuration, including the sheet details once createdd
-- `credentials.json`: Google API credentials (copied from `-c`/`--credentials`)
-- `field.tsv`: Sheet field names (contains default COGS fields)
-- `sheet.tsv`: Sheet names and details (empty)
-- `table.tsv`: Table names and details (empty)
-- `users.tsv`: User emails and roles (filled out from `-u`/`--user` and `-U`/`--users`)
-
-### `create`
+Running `init` creates a `.cogs` directory containing configuration data. This also creates a new Google Sheet and stores the Sheet ID. Optionally, this new sheet may be shared with users.
 
 ```
-cogs create -t [title] -d [description]
+cogs init -c [path-to-credentials] -t [project-title] -u [email] -r [role]
 ```
 
-Running `create` will create a new, empty, Google sheet with the given title (`-t`/`--title`). A description (`-d`/`--description`) is optional.
+Options:
+- `-c`/`--credentials`: **required**, path to Google API credentials](https://gspread.readthedocs.io/en/latest/oauth2.html#enable-api-access-for-a-project) in JSON format
+- `-t`/`--title`: **required**, title of the project which will be used as the title of the Google Sheet
+- `-u`/`--user`: email of the user to share the sheet with (if a `--role` is not specified, this user will be the owner of the Sheet)
+- `-r`/`--role`: role of the user specified by `--user`: `owner`, `writer`, `viewer`
+- `-U`/`--users`: path to TSV containing emails and roles for multiple users (no header - only one `owner` should be specified)
 
-This sheet will automatically be shared with any users specified in `init` and these details will be added to `.cogs/sheet.tsv`.
+Three files are created in the `.cogs/` directory when running `init`:
+- `config.tsv`: COGS configuration, including the Sheet details 
+- `field.tsv`: Field names used in tables (contains default COGS fields)
+- `sheet.tsv`: Table names in Sheet and details (empty) - the tables correspond to tabs in the Sheet
--- src/cogs/cli.py
+++ src/cogs/cli.py
@@ -9,7 +9,17 @@ import sys
 from argparse import ArgumentParser
 
 
-def validate():
+def get_config():
+    """Get the configuration for this project as a dict."""
+    config = {}
+    with open(".cogs/config.tsv", "r") as f:
+        reader = csv.reader(f, delimiter="\t", lineterminator="\n")
+        for row in reader:
+            config[row[0]] = row[1]
+    return config
+
+
+def is_cogs_project():
     """Validate that there is a valid COGS project in this directory."""
     if not os.path.exists(".cogs/") or not os.path.isdir(".cogs/"):
         print("ERROR: A COGS project has not been initialized!")
@@ -23,101 +33,69 @@ def validate():
     return True
 
 
-def create(args):
-    """Create a new Google Sheet and add it to existing project."""
-    if not validate():
-        sys.exit(1)
-
-    gc = gspread.service_account(filename=".cogs/credentials.json")
-
-    # Maybe get emails to share sheet with
-    emails = {}
-    with open(".cogs/user.tsv") as f:
-        next(f)
-        for line in f:
-            e = line.split("\t")[0].strip()
-            role = line.split("\t")[1].strip().lower()
-            emails[e] = role
-
-    # Create the sheet and share
-    print(f"Creating new sheet: {args.title}")
-    sh = gc.create(args.title)
-    for e, role in emails.items():
-        print(f"Sharing {args.title} with {e}")
-        sh.share(e, perm_type="user", role=role)
-
-    # Write details to sheet TSV
-    with open(".cogs/sheet.tsv", "a") as f:
-        writer = csv.writer(f, delimiter="\t", lineterminator="\n")
-        desc = ""
-        if args.description:
-            desc = args.description
-        writer.writerow([sh.id, args.title, desc])
-
-
 def init(args):
     """Init a new .cogs configuration directory in the current working directory. If one already
     exists, reinit it by rewriting config.tsv and ensuring that sheet.tsv and field.tsv exist."""
     cwd = os.getcwd()
-    if not os.path.exists(".cogs"):
-        print(f"Initializing COGS configuration in {cwd}/.cogs/")
-        os.mkdir(".cogs")
-    else:
-        print(f"Reinitializing existing COGS configuration in {cwd}/.cogs/")
+    if os.path.exists(".cogs"):
+        print(f"ERROR: COGS project already exists in {cwd}/.cogs/")
+        sys.exit(1)
 
-    # Store COGS configuration
-    # If this already exists, it *will* be overwritten
-    with open(".cogs/config.tsv", "w") as f:
-        writer = csv.writer(f, delimiter="\t", lineterminator="\n")
-        v = pkg_resources.require("COGS")[0].version
-        writer.writerow(["COGS", "https://github.com/ontodev/cogs"])
-        writer.writerow(["COGS Version", v])
+    print(f"Initializing COGS project '{args.title}' in {cwd}/.cogs/")
+    os.mkdir(".cogs")
 
-    # Store credentials
-    with open(args.credentials, "r") as fr:
-        with open(".cogs/credentials.json", "w") as fw:
-            for line in fr:
-                fw.write(line)
+    # Create the sheet for this project
+    gc = gspread.service_account(args.credentials)
+    sh = gc.create(args.title)
 
-    # Init sheet.tsv, table.tsv, field.tsv, and user.tsv
-    # If these already exist, they will not be overwritten (unless they are empty)
-    if not os.path.exists(".cogs/sheet.tsv") or os.stat(".cogs/sheet.tsv").st_size == 0:
-        with open(".cogs/sheet.tsv", "w") as f:
-            writer = csv.writer(f, delimiter="\t", lineterminator="\n")
-            writer.writerow(["Sheet ID", "Sheet Title", "Description"])
-
-    if not os.path.exists(".cogs/table.tsv") or os.stat(".cogs/table.tsv").st_size == 0:
-        with open(".cogs/table.tsv", "w") as f:
-            writer = csv.writer(f, delimiter="\t", lineterminator="\n")
-            writer.writerow(
-                ["Table ID", "Table Title", "Sheet ID", "Path", "Description"]
-            )
-
-    if not os.path.exists(".cogs/field.tsv") or os.stat(".cogs/field.tsv").st_size == 0:
-        with open(".cogs/field.tsv", "w") as f:
-            writer = csv.writer(f, delimiter="\t", lineterminator="\n")
-            writer.writerow(["Field", "Label", "Datatype", "Description"])
-            writer.writerows(default_fields)
-
-    if not os.path.exists(".cogs/user.tsv") or os.stat(".cogs/user.tsv").st_size == 0:
-        with open(".cogs/user.tsv", "w") as f:
-            writer = csv.writer(f, delimiter="\t", lineterminator="\n")
-            writer.writerow(["User", "Role"])
-
-    # Maybe add users
+    # Maybe get users and share new sheet with them
+    users = {}
     if args.user:
-        # Default role is owner (all access)
+        # Default role is owner
         role = "owner"
         if args.role:
             role = args.role
-        with open(".cogs/user.tsv", "a") as f:
-            f.write(f"{args.user}\t{role}\n")
-
+        users[args.user] = role
     if args.users:
-        with open(args.users, "r") as fr:
-            emails = fr.read()
-            with open(".cogs/user.tsv", "a") as fw:
-                fw.write(emails)
+        with open(args.users, "r") as f:
+            reader = csv.reader(f, delimiter="\t")
+            for row in reader:
+                users[row[0]] = row[1]
+    for email, role in users.items():
+        sh.share(email, perm_type="user", role=role)
+
+    # Store COGS configuration
+    with open(".cogs/config.tsv", "w") as f:
+        writer = csv.DictWriter(
+            f, delimiter="\t", lineterminator="\n", fieldnames=["Key", "Value"]
+        )
+        v = pkg_resources.require("COGS")[0].version
+        writer.writerow({"Key": "COGS", "Value": "https://github.com/ontodev/cogs"})
+        writer.writerow({"Key": "COGS Version", "Value": v})
+        writer.writerow({"Key": "Credentials", "Value": args.credentials})
+        writer.writerow({"Key": "Title", "Value": args.title})
+        writer.writerow({"Key": "ID", "Value": sh.id})
+
+    # sheet.tsv contains table (tab) details from the Google Sheet
+    with open(".cogs/sheet.tsv", "w") as f:
+        writer = csv.DictWriter(
+            f,
+            delimiter="\t",
+            lineterminator="\n",
+            fieldnames=["ID", "Title", "Path", "Description"],
+        )
+        writer.writeheader()
+
+    # field.tsv contains the field headers used in the sheets
+    with open(".cogs/field.tsv", "w") as f:
+        writer = csv.DictWriter(
+            f,
+            delimiter="\t",
+            lineterminator="\n",
+            fieldnames=["Field", "Label", "Datatype", "Description"],
+        )
+        writer.writeheader()
+        writer.writerows(default_fields)
     sys.exit(0)
 
 
@@ -139,6 +117,7 @@ def main():
     sp.add_argument(
         "-c", "--credentials", required=True, help="Path to service account credentials"
     )
+    sp.add_argument("-t", "--title", required=True, help="Title of the project")
     sp.add_argument("-u", "--user", help="Email to share all sheets with")
     sp.add_argument(
         "-r", "--role", help="Role for user specified in email (default: writer)"
@@ -146,32 +125,50 @@ def main():
     sp.add_argument("-U", "--users", help="TSV containing user emails and their roles")
     sp.set_defaults(func=init)
 
-    sp = subparsers.add_parser("create")
-    sp.add_argument("-t", "--title", required=True, help="Title of the new spreadsheet")
-    sp.add_argument(
-        "-d", "--description", help="Description of the contents of this sheet"
-    )
-    sp.set_defaults(func=create)
-
     args = parser.parse_args()
     args.func(args)
 
 
 default_fields = [
-    ["sheet", "Sheet", "cogs:sql_id", "The identifier for this sheet"],
-    ["label", "Label", "cogs:label", "The label for this row"],
-    [
-        "file_path",
-        "File Path",
-        "cogs:file_path",
-        "The relative path of the TSV file for this sheet",
-    ],
-    ["description", "Description", "cogs:text", "A description of this row"],
-    ["field", "Field", "cogs:sql_id", "The identifier for this field"],
-    ["datatype", "Datatype", "cogs:curie", "The datatype for this row"],
+    {
+        "Field": "sheet",
+        "Label": "Sheet",
+        "Datatype": "cogs:sql_id",
+        "Description": "The identifier for this sheet",
+    },
+    {
+        "Field": "label",
+        "Label": "Label",
+        "Datatype": "cogs:label",
+        "Description": "The label for this row",
+    },
+    {
+        "Field": "file_path",
+        "Label": "File Path",
+        "Datatype": "cogs:file_path",
+        "Description": "The relative path of the TSV file for this sheet",
+    },
+    {
+        "Field": "description",
+        "Label": "Description",
+        "Datatype": "cogs:text",
+        "Description": "A description of this row",
+    },
+    {
+        "Field": "field",
+        "Label": "Field",
+        "Datatype": "cogs:sql_id",
+        "Description": "The identifier for this field",
+    },
+    {
+        "Field": "datatype",
+        "Label": "Datatype",
+        "Datatype": "cogs:curie",
+        "Description": "The datatype for this row",
+    },
 ]
 
-reqs = ["credentials.json", "user.tsv", "sheet.tsv", "table.tsv", "config.tsv"]
+reqs = ["user.tsv", "sheet.tsv", "field.tsv", "config.tsv"]
 
 
 if __name__ == "__main__":
