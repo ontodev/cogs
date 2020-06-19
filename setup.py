5c7579a679cc5e71ad8e1affd65926a94b20c07c
rctauber <rctauber@gmail.com>
2020-06-19 08:27:34 -0700

    Add minimal requirements to setup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     python_requires='>=3.6, <4',
-    install_requires=[],
+    install_requires=["google", "gspread"],
     entry_points={
         "console_scripts": [
             "cogs = cogs.cli:main",
