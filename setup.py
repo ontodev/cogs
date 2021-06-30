e7c5b95839ee5dc010a1749646dfea5b01b07cc2
Becky Jackson <rbca.jackson@gmail.com>
2021-06-30 07:48:55 -0700

    Bump to v0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(here + "/README.md", "r") as fh:
 
 setup(
     name="ontodev-cogs",
-    version="0.3.1",
+    version="0.3.2",
     description="COGS Operates Google Sheets",
     long_description=long_description,
     long_description_content_type="text/markdown",
